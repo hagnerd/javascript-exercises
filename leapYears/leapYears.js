1aa4443812c7940cda348c0a12904c376691425c
Matt Hagner <web@matthagner.com>
2018-01-24 07:00:47 -0600

    Solve leapYear.spec.js

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,6 @@
 var leapYears = function(year) {
-  return year % 4 === 0 ? true : false;
+  return (year % 100 === 0 && year % 400 != 0) ? false :
+    year % 4 === 0 ? true : false;
 }
 
 module.exports = leapYears
--- leapYears/leapYears.spec.js
+++ leapYears/leapYears.spec.js
@@ -10,13 +10,13 @@ describe('leapYears', function() {
   it('works with ridiculously futuristic non century years', function() {
     expect(leapYears(34992)).toEqual(true);
   });
-  xit('works with century years', function() {
+  it('works with century years', function() {
     expect(leapYears(1900)).toEqual(false);
   });
-  xit('works with century years', function() {
+  it('works with century years', function() {
     expect(leapYears(1600)).toEqual(true);
   });
-  xit('works with century years', function() {
+  it('works with century years', function() {
     expect(leapYears(700)).toEqual(false);
   });
 });
