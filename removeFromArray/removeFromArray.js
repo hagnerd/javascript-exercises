37af0ee7e864f86fbcd15835185ff41e692d3d64
Matt Hagner <web@matthagner.com>
2018-01-23 21:56:08 -0600

    Solve removeFromArray.js

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-var removeFromArray = function() {
+var removeFromArray = function(arr, el) {
+  let args = arguments;
 
+  for(let i=1; i < args.length; i++){
+    if(arr.includes(args[i])) { arr.splice(arr.indexOf(args[i]), 1) }
+  }
+
+  return arr
 }
 
 module.exports = removeFromArray
